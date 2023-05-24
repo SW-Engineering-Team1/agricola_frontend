4ab4a5e1eddc3e7c47f4dd27b0cbdf44fac552b3
Anyohan <dygks3240@hanyang.ac.kr>
2023-05-24 15:57:52 +0900

    Register
    회원가입

--- src/views/RegisterForm.vue
+++ src/views/RegisterForm.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex items-center justify-center h-screen">
         <div class="container mx-auto max-w-sm bg-gray-300">
-        <form @submit="login">
+        <form @submit.prevent="register">
         <div class="input mb-6">
             <h2 class="text-xl font-bold mb-2">Register</h2>
-            <label for="id" class="mb-2">Username</label>
-            <input type="text" name="username" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="username" />
+            <label for="uid" class="mb-2">Username</label>
+            <input type="text" v-model ="uid" name="uid" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="username" />
             <br>
             <label for="password" class="mb-2">Password</label>
-            <input type="password" name="password" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="password" />
+            <input type="password" v-model="password" name="password" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="password" />
         </div>
         <div class="mb-4">
             Already have an account?
@@ -21,8 +21,6 @@
 </template>
 
 <script>
-// import axios from 'axios'
-
 export default {
     name: 'RegisterPage',
     data() {
@@ -33,20 +31,26 @@ export default {
     },
 
     methods: {
-       async register() {
-            try {
+        async register() {
                 const userData = {
-                    uid: this.uid,
+                    id: this.uid,
                     password: this.password
                 };
-                const response = await this.$axios.post('http://localhost:3000/user/signup', userData);
-                console.log(response.data);
-            } catch (error) {
-                console.error(error);
-            }
+                this.$axios.post('http://localhost:3000/user/signup', userData).then(function (res) {
+                console.log(res);
+                if (res.status == 200) {
+                    alert('회원 가입 완료되었습니다.');
+                    this.$router.push('/');
+                }
+                else {
+                    alert('회원 가입 실패하였습니다.');
+                }
+                }).catch(function (err) {
+                    console.log(err);
+                });
         },
         moveToLogin() {
-            this.$router.push('/')
+            this.$router.push('/');
         },
     },
 };
