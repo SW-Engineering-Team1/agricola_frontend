fa897375ba50181eef70d3143c92aa693884de02
dev-jjjjjeong-bin <86920724+dev-jjjjjeong-bin@users.noreply.github.com>
2023-05-30 16:27:10 +0900

    [CHORE] router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App'
 import router from './router'
-import store from "./store/sotrage"
+import store from './store/store.js'
 import './assets/styles.css'
 import axios from 'axios'
 
