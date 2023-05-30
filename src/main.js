06c819fa4f3ddd674f15d1d83bf7b1b7716ddb04
dev-jjjjjeong-bin <86920724+dev-jjjjjeong-bin@users.noreply.github.com>
2023-05-30 16:03:15 +0900

    vuex install

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App'
 import router from './router'
+import store from "./store/sotrage"
 import './assets/styles.css'
 import axios from 'axios'
 
@@ -8,3 +9,8 @@ const app = createApp(App)
 
 app.config.globalProperties.$axios = axios
 app.use(router).mount('#app')
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app');
