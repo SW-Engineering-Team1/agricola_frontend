0968b0b5822f08ccb6aa57bb487c4593976687d8
Seung-ho Kim <rhanfwlq980@naver.com>
2023-06-03 22:46:18 +0900

    [FEAT] 로비 페이지 디자인 깔끔하게 수정

--- src/views/LobbyPage.vue
+++ src/views/LobbyPage.vue
@@ -1,63 +1,80 @@
 <template>
-  <div id="app">
-    <h1 class="text-4xl">로비</h1>
-    <h2>로그인한 유저 : {{ user }}</h2>
-    <div class="bg-red-300 p-5">
-      <button @click="showModal = true" class="bg-blue-300 mx-80 p-2 my-5">
-        방 만들기
-      </button>
+  <div id="app" class="font-sans antialiased text-gray-900">
+    <div class="container mx-auto px-4">
+      <h1 class="text-4xl text-center my-5">로비</h1>
+      <h2 class="text-lg text-center mb-5">로그인한 유저 : {{ user }}</h2>
 
-      <!-- 방 만들기 모달 창 -->
-      <div v-if="showModal" class="modal">
-        <div class="modal-content">
-          <span class="close" @click="showModal = false">&times;</span>
-          <div>
-            <h1 class="text-4xl">방 만들기</h1>
-            <div>
-              <label for="roomName">방 이름</label>
-              <input v-model="roomName" placeholder="방 이름을 작성해주세요.">
+      <div class="bg-gray-300 p-5 rounded-lg shadow-lg">
+        <div class="flex justify-center">
+          <button @click="showModal = true" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            방 만들기
+          </button>
+        </div>
+
+        <!-- 방 만들기 모달 창 -->
+        <div v-if="showModal" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+          <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+
+            <!-- Modal content -->
+            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div class="sm:flex sm:items-start">
+                  <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 class="text-4xl leading-6 font-medium text-gray-900" id="modal-title">
+                      방 만들기
+                    </h3>
+                    <div class="mt-2">
+                      <label for="roomName">방 이름</label>
+                      <input v-model="roomName" placeholder="방 이름을 작성해주세요." class="mt-1 p-1 block w-full rounded-md border-gray-300 focus:ring-indigo-500">
+                    </div>
+                    <div class="mt-2">
+                      <label for="playerCount">플레이어 수</label>
+                      <select v-model="limitNum" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <option value="1">1명</option>
+                        <option value="2">2명</option>
+                        <option value="3">3명</option>
+                        <option value="4">4명</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button @click="createRoom" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
+                  확인
+                </button>
+                <button @click="showModal = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                  취소
+                </button>
+              </div>
             </div>
-            <div>
-              <label for="playerCount">플레이어 수</label>
-              <select v-model="limitNum">
-                <option value="1">1명</option>
-                <option value="2">2명</option>
-                <option value="3">3명</option>
-                <option value="4">4명</option>
-              </select>
-            </div>
-          </div>
-          <!-- 확인 및 취소 버튼 -->
-          <div class="flex justify-center">
-            <button @click="createRoom" class="bg-blue-300 mx-2 p-2">확인</button>
-            <button @click="showModal = false" class="bg-blue-300 mx-2 p-2">취소</button>
           </div>
         </div>
-      </div>
 
-      <!-- 방 목록 -->
-      <!-- 방 고유 아이디, 방 이름, 현재 인원 수, 제한 인원, 게임 상태, 방장의 닉네임 -->
-      <div class="flex flex-wrap justify-center">
-        <div
-          v-for="room in rooms"
-          :key="room.room_id"
-          class="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          <!-- TODO: 방 입장 기능 개발 -->
-          <button
-            @click="joinRoom(room.room_id)"
-            class="block text-center">
-            {{ room.room_name }} ({{ room.participant_num }} / {{ room.limit_num }})
-            <br>
-            게임 상태 : {{ room.status }}
-            <br>
-            방장 : {{ room.host_id }}
-          </button>
+        <!-- 방 목록 -->
+        <div class="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div
+              v-for="room in rooms"
+              :key="room.room_id"
+              class="flex flex-col bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg shadow-md">
+            <button
+                @click="joinRoom(room.room_id)"
+                class="text-center">
+              {{ room.room_name }} ({{ room.participant_num }} / {{ room.limit_num }})
+              <br>
+              게임 상태 : {{ room.status }}
+              <br>
+              방장 : {{ room.host_id }}
+            </button>
+          </div>
         </div>
       </div>
     </div>
     <router-view/>
   </div>
 </template>
+
 
 <script>
 import { ref, onMounted, onUnmounted, computed } from 'vue'
@@ -146,41 +163,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.modal {
-  display: block; 
-  position: fixed; 
-  z-index: 1; 
-  padding-top: 100px; 
-  left: 0;
-  top: 0;
-  width: 100%; 
-  height: 100%; 
-  overflow: auto; 
-  background-color: rgb(0,0,0); 
-  background-color: rgba(0,0,0,0.4); 
-}
-
-.modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-.close {
-  color: #aaaaaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
-}
-</style>
