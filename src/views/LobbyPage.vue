bb339797bb5bd0ce1482942c470da0f24470604c
Seung-ho Kim <rhanfwlq980@naver.com>
2023-06-04 21:20:29 +0900

    [REFACTOR] 방 입장 컴포넌트화

--- src/views/LobbyPage.vue
+++ src/views/LobbyPage.vue
@@ -16,22 +16,10 @@
           @createRoom="handleCreateRoom"
         />
         <!-- 방 목록 -->
-        <div class="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <div
-              v-for="room in rooms"
-              :key="room.room_id"
-              class="flex flex-col bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg shadow-md">
-            <button
-                @click="joinRoom(room.room_id)"
-                class="text-center">
-              {{ room.room_name }} ({{ room.participant_num }} / {{ room.limit_num }})
-              <br>
-              게임 상태 : {{ room.status }}
-              <br>
-              방장 : {{ room.host_id }}
-            </button>
-          </div>
-        </div>
+        <RoomList
+          :rooms="rooms"
+          @joinRoom="handleJoinRoom"
+        />
       </div>
     </div>
     <router-view/>
@@ -47,10 +35,11 @@ import { io } from "socket.io-client";
 import axios from 'axios'
 import CreateRoomButton from '@/components/CreateRoomButton.vue'
 import CreateRoomModal from '@/components/CreateRoomModal.vue'
+import RoomList from '@/components/RoomList.vue'
 
 export default {
   name: 'LobbyPage',
-  components: {CreateRoomModal, CreateRoomButton},
+  components: {RoomList, CreateRoomModal, CreateRoomButton},
 
   setup() {
     const router = useRouter();
@@ -112,7 +101,6 @@ export default {
       rooms,
       handleCreateRoom,
       handleJoinRoom,
-      joinRoom,
       user,
       showModal
     }
