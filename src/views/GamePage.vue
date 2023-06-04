e151f96a5dac0a0a1827347b526be7bdab503c51
dev-jjjjjeong-bin <86920724+dev-jjjjjeong-bin@users.noreply.github.com>
2023-06-04 18:46:57 +0900

    [ADD] GamePage - AssiFacModal, JobCardModal

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -1,28 +1,68 @@
 <template>
   <div>
-    <MajorFacModal v-if="isModalOpen" @close-modal="closeModal"></MajorFacModal>
-    <button class="rounded" @click="openModal">주요설비창</button>
+    <MajorFacModal v-if="isMajorFacModalOpen" @close-modal="closeMajorFacModal"></MajorFacModal>
+    <button class="rounded" @click="openMajorFacModal">주요설비창</button>
+
+    <AssiFacModal v-if="isAssiFacModalOpen" @close-modal="closeAssiFacModal"></AssiFacModal>
+    <button class="rounded" @click="openAssiFacModal">보조설비창</button>
+
+    <JobCardModal v-if="isJobCardModalOpen" @close-modal="closeJobCardModal"></JobCardModal>
+    <button class="rounded" @click="openJobCardModal">직업카드창</button>
   </div>
 </template>
+
 <script>
 import MajorFacModal from '@/components/MajorFacModal.vue';
+import AssiFacModal from '@/components/AssiFacModal.vue';
+import JobCardModal from '@/components/JobCardModal.vue';
 import { ref } from 'vue';
 
 export default {
   components: {
     MajorFacModal,
+    AssiFacModal,
+    JobCardModal,
   },
   setup() {
-    const isModalOpen = ref(false);
+    const isMajorFacModalOpen = ref(false);
+    const isAssiFacModalOpen = ref(false);
+    const isJobCardModalOpen = ref(false);
 
-    const openModal = () => {
-      isModalOpen.value = true;
-    };
-    const closeModal = () => {
-      isModalOpen.value = false;
+    const openMajorFacModal = () => {
+      isMajorFacModalOpen.value = true;
     };
 
-    return { isModalOpen, openModal, closeModal };
+    const closeMajorFacModal = () => {
+      isMajorFacModalOpen.value = false;
+    };
+
+    const openAssiFacModal = () => {
+      isAssiFacModalOpen.value = true;
+    };
+
+    const closeAssiFacModal = () => {
+      isAssiFacModalOpen.value = false;
+    };
+
+    const openJobCardModal = () => {
+      isJobCardModalOpen.value = true;
+    };
+
+    const closeJobCardModal = () => {
+      isJobCardModalOpen.value = false;
+    };
+
+    return {
+      isMajorFacModalOpen,
+      isAssiFacModalOpen,
+      isJobCardModalOpen,
+      openMajorFacModal,
+      closeMajorFacModal,
+      openAssiFacModal,
+      closeAssiFacModal,
+      openJobCardModal,
+      closeJobCardModal,
+    };
   },
 };
 </script>
