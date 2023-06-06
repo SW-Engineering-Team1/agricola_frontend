f286ad3af3813dfaacbfdc6365e48d5f5ec80ff1
dev-jjjjjeong-bin <86920724+dev-jjjjjeong-bin@users.noreply.github.com>
2023-06-06 18:05:19 +0900

    [ADD] GamePage Images
    일단 위치 보라고 올림

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
     <MajorFacModal v-if="isMajorFacModalOpen" @close-modal="closeMajorFacModal"></MajorFacModal>
-    <button class="rounded major-fac" @click="openMajorFacModal">주요설비</button>
+    <button class="rounded major-fac" @click="openMajorFacModal" style="background: transparent; border: none; padding: 0;">
+      <img src="../assets/images/MajorFacCardBack/MajorFacCardBack.png" width="90">
+    </button>
 
     <P1AssiFacModal v-if="isP1AssiFacModalOpen" @close-modal="closeP1AssiFacModal"></P1AssiFacModal>
-    <button class="rounded p1-assi-fac" @click="openP1AssiFacModal">P1보조설비</button>
+    <button class="rounded p1-assi-fac" @click="openP1AssiFacModal" style="background: transparent; border: none; padding: 0;">
+      <img src="../assets/images/AssiFacCardBack/P1AssiFacCardBack.png" width="90">
+    </button>
 
     <P1JobCardModal v-if="isP1JobCardModalOpen" @close-modal="closeP1JobCardModal"></P1JobCardModal>
-    <button class="rounded p1-job-card" @click="openP1JobCardModal">P1직업카드</button>
+    <button class="rounded p1-job-card" @click="openP1JobCardModal" style="background: transparent; border: none; padding: 0;">
+      <img src="../assets/images/JobCardBack/P1JobCardBack.png" width="90">
+    </button>
 
     <P2AssiFacModal v-if="isP2AssiFacModalOpen" @close-modal="closeP2AssiFacModal"></P2AssiFacModal>
-    <button class="rounded p2-assi-fac" @click="openP2AssiFacModal">P2보조설비</button>
+    <button class="rounded p2-assi-fac" @click="openP2AssiFacModal" style="background: transparent; border: none; padding: 0;">
+      <img src="../assets/images/AssiFacCardBack/P2AssiFacCardBack.png" width="90">
+    </button>
 
     <P2JobCardModal v-if="isP2JobCardModalOpen" @close-modal="closeP2JobCardModal"></P2JobCardModal>
-    <button class="rounded p2-job-card" @click="openP2JobCardModal">P2직업카드</button>
+    <button class="rounded p2-job-card" @click="openP2JobCardModal" style="background: transparent; border: none; padding: 0;">
+      <img src="../assets/images/JobCardBack/P2JobCardBack.png" width="90">
+    </button>
+    
+    <div class="MainGameBoard"><img src="../assets/images/MainGameBoard/MainGameBoard.png" width="800"></div>
+    <div class="P1FarmBoard"><img src="../assets/images/FarmBoard/P1FarmBoard.png" width="350"></div>
+    <div class="P2FarmBoard"><img src="../assets/images/FarmBoard/P2FarmBoard.png" width="350"></div>
   </div>
 </template>
 
@@ -95,30 +109,42 @@ export default {
 <style>
 .major-fac {
   position: absolute;
-  top: 50%;
-  right: 40%;
+  top: 35%;
+  right: 20%;
 }
-
 .p1-assi-fac {
   position: absolute;
   bottom: 5%;
-  right: 15%;
+  right: 30%;
 }
-
 .p1-job-card {
   position: absolute;
   bottom: 5%;
-  right: 5%;
+  right: 25%;
 }
 .p2-assi-fac {
   position: absolute;
   top: 5%;
-  right: 15%;
+  right: 30%;
 }
-
 .p2-job-card {
   position: absolute;
   top: 5%;
-  right: 5%;
+  right: 25%;
+}
+.MainGameBoard {
+  position: absolute;
+  top: 28%;
+  right: 27%;
+}
+.P1FarmBoard {
+  position: absolute;
+  bottom: 0%;
+  right: 40%;
+}
+.P2FarmBoard {
+  position: absolute;
+  top: 0%;
+  right: 40%;
 }
 </style>
