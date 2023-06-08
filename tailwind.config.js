968520962d5cf04b7c70a5a5eafc2581bf9c557f
Seung-ho Kim <rhanfwlq980@naver.com>
2023-06-08 21:25:42 +0900

    [UPDATE] TailwindCSS 버전 2.2에서 3.3 으로 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js, vue}"],
+  purge: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
