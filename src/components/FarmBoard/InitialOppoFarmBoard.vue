f8a4d821147ac2fba761a62183c7df0f18aa683f
dev-jjjjjeong-bin <86920724+dev-jjjjjeong-bin@users.noreply.github.com>
2023-06-11 23:54:15 +0900

    [FIX] Initial FarmBoard

--- src/components/FarmBoard/InitialOppoFarmBoard.vue
+++ src/components/FarmBoard/InitialOppoFarmBoard.vue
@@ -1,24 +1,64 @@
 <!-- 게임 시작 시 상대 농장판 -->
 <template>
   <div class="flex justify-center">
-    <div class="bg-green-700 grid grid-cols-5 gap-2 p-2 rotate-180">
+    <div class="bg-green-700 grid grid-cols-5 gap-2 p-2">
       <img
-        v-for="farm in oppoFarm"
+        v-for="farm in OppoFarm"
         :key="farm.id"
         :src="farm.imgSrc"
         alt="oppoFarm"
-        class="w-16 h-16"
+        :class="[ 'w-16 h-16' ]"
       />
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import { farmRef } from '@/constants';
 export default {
+  setup() {
+    const oppoFarm = ref(farmRef);
+    // oppoFarm을 위한 함수들을 동적으로 생성
+    const oppoFarmFunctions = {};
+    for (let i = 1; i <= 15; i++) {
+      const oppoFarmName = `openOppoFarm${i}`;
+      oppoFarmFunctions[oppoFarmName] = () => {
+        console.log(oppoFarmName);
+      };
+      // 해당 oppoFarm의 clickHandler를 등록
+      for (const farm of oppoFarm.value) {
+        farm.clickHandler = oppoFarmFunctions[`openOppoFarm${farm.id}`];
+      }
+    }
+    return {
+        oppoFarm
+    };
+  },
+  
   name: "OppoFarm",
-  //*  상대 농장판 데이터를 받아오는 prop */
   props: {
-    oppoFarm: Array,
+    OppoFarm: {
+      type: Array,
+      required: true,
+      default: () => [
+        { id: 1, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 2, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 3, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 4, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 5, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 6, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 7, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 8, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 9, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 10, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 11, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 12, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 13, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 14, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+        { id: 15, imgSrc: require('@/assets/images/Farm/Farm.jpg') },
+      ]
+    }
   },
 };
 </script>
