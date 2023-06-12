1d265dce51b45fa56fa296ec12f799341a332352
dev-jjjjjeong-bin <86920724+dev-jjjjjeong-bin@users.noreply.github.com>
2023-06-12 23:41:57 +0900

    [ADD] BasicAction9 - ReedField

--- src/components/BasicActions/ReedField.vue
+++ src/components/BasicActions/ReedField.vue
@@ -1,15 +1,21 @@
-<!-- Action9 갈대밭 행동칸 -->
+<!--  BasicAction9 갈대밭 행동칸 -->
 <template>
   <div>
     <img
       src="@/assets/images/Action/9_ReedField.jpg"
       @click="handleClick(isMyTurn)"
       :class="{'w-full cursor-pointer transform transition duration-500 ease-in-out hover:scale-110': true, 'pointer-events-none': !isMyTurn}"
-      alt="reedField"
+      alt="ReedField"
     />
   </div>
 </template>
+
 <script>
+import { computed, onMounted, ref } from "vue";
+import { io } from "socket.io-client";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+
 export default {
   props: {
     isMyTurn: {
@@ -18,11 +24,36 @@ export default {
     },
   },
   setup() {
+    const socket = io("localhost:3000");
+    const store = useStore();
+    const route = useRoute();
+
+    const roomId = ref("");
+    const user = computed(() => store.state.user);
+    const reedAccumulated = ref(computed(() => store.state.accumulatedResources.reedAccumulated));
+
+    const useReedField = () => {
+      socket.emit("useActionSpace", {
+        actionName: "Use Accumulated Goods",
+        userId: user.value,
+        roomId: roomId.value,
+        goods: [
+          {
+            name: "reedAccumulated",
+            num: reedAccumulated.value,
+            isAdd: true,
+          },
+        ],
+      });
+    };
+
+    onMounted(async () => {
+      roomId.value = route.params.room;
+    });
 
     const handleClick = (isMyTurn) => {
       if (isMyTurn) {
-        // 아무것도 하지 않음
-        console.log("갈대밭");
+        useReedField();
       }
     }
 
